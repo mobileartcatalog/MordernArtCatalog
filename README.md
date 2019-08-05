79f747e5f2966ea9530cc6eeecf352c6bfea2efc
Xiaoyu Zhu <emmazxy@gmail.com>
2019-08-05 11:08:59 -0400

    after stage

--- README.md
+++
@@ -1,8 +0,0 @@
-
-#1 npm install 
-
-#2 create default.json in config folder. add mongoDBURI into the file.
-
-#3 npm run start-dev , starting build watch and ready to developer
-
-
--- client/app.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-ReactDOM.render(
-  <div>Hello from React! Begin to creat the App!</div>,
-  document.getElementById('app')
-);
